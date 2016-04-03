1c12ec7570e3ec883dd0d1cd9c5e2044ebe5f38d
Nicolas Oliveira <nk.oliveira@gmail.com>
2016-04-03 11:57:57 -0300

    Incluindo autenticação básica
    lembrete para autenticação! cabeçalho HTTP: Authorization 'Basic
    B64(usuario:senha) '

---
+++ src/main/java/com/aftermidnight/socialbooks/config/WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.aftermidnight.socialbooks.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("AUTHENTICATED");
+	}
+	
+	protected void configure(HttpSecurity http) throws Exception{
+		http
+		.authorizeRequests()
+		.antMatchers("/h2-console/**").permitAll() //a uri /h2-console vai bypassar a autenticação
+		.anyRequest().authenticated() //todas as requisições devem ser autenticadas
+			.and()
+				.httpBasic() //metodo de autenticação (Podem ser outros)
+			.and()
+				.csrf().disable(); //proteção para evitar ataques csrf
+	}
+}
